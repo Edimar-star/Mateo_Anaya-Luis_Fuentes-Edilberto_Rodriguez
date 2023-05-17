d825710396e93757c7dc138465bcf1ac4718a625
luisda190519 <licerol@uninorte.edu.co>
2023-05-16 20:29:38 -0500

    Changes

--- trie.py
+++ trie.py
@@ -1,36 +1,40 @@
 class TrieNode:
     def __init__(self):
-        self.children = {}
-        self.is_end_of_word = False
+        # Inicializa un nodo Trie
+        self.children = {}  # Diccionario para almacenar los hijos del nodo
+        self.is_end_of_word = False  # Bandera para indicar si es el final de una palabra
 
 def insert_prefixes(prefixes):
-    root = TrieNode()
+    # Inserta los prefijos en un Trie y devuelve la raíz del Trie
+    root = TrieNode()  # Crea un nodo raíz vacío del Trie
     for prefix in prefixes:
-        node = root
+        node = root  # Comienza desde la raíz para cada prefijo
         for char in prefix:
             if char not in node.children:
-                node.children[char] = TrieNode()
-            node = node.children[char]
-        node.is_end_of_word = True
+                node.children[char] = TrieNode()  # Crea un nuevo nodo si el carácter no está presente en los hijos del nodo actual
+            node = node.children[char]  # Avanza al siguiente nodo hijo
+        node.is_end_of_word = True  # Marca el último nodo como el final de una palabra
 
     return root
 
 def remove_prefixes(cadena, root):
+    # Remueve los prefijos de una cadena utilizando el Trie y devuelve el resultado
     resultado = []
     while cadena:
-        node = root
-        prefix_found = False
+        node = root  # Comienza desde la raíz para cada cadena
+        prefix_found = False  # Bandera para indicar si se encontró un prefijo
         for i, char in enumerate(cadena):
             if char not in node.children:
-                resultado.append(cadena)
+                resultado.append(cadena)  # Agrega la cadena completa si no se encuentra el carácter en los hijos del nodo actual
                 prefix_found = False
                 break
-            node = node.children[char]
+            node = node.children[char]  # Avanza al siguiente nodo hijo
             if node.is_end_of_word:
-                resultado.append(cadena[i+1:])
+                resultado.append(cadena[i+1:])  # Agrega la subcadena restante si se encuentra un prefijo
                 prefix_found = True
                 break
         if not prefix_found:
             break
-        cadena = resultado[-1]
-    return resultado[-1]
+        cadena = resultado[-1]  # Actualiza la cadena con la última subcadena agregada
+    return resultado[-1]  # Devuelve la última subcadena resultante
+
--- verificador.py
+++ verificador.py
@@ -2,23 +2,32 @@ import sys
 import os
 
 if __name__ == "__main__":
+    # Se obtienen los nombres de archivo de los argumentos de línea de comandos
     filename = sys.argv[1]
     decompressed_filename = sys.argv[2]
 
+    # Se obtiene el tamaño en bytes de los archivos
     size1 = os.path.getsize(filename)
     size2 = os.path.getsize(decompressed_filename)
     diff = size2 - size1
     
+    # Se abren los archivos en modo lectura binaria y se leen los contenidos
     with open(filename, "rb") as archivo1, open(decompressed_filename, "rb") as archivo2:
         try:
+            # Se intenta decodificar los contenidos de los archivos utilizando "cp1252"
             archivo1 = archivo1.read().decode("cp1252")
             archivo2 = archivo2.read().decode("cp1252")
         except:
+            # Si falla la decodificación con "cp1252", se intenta con "utf-8"
             archivo1 = archivo1.read().decode("utf-8")
             archivo2 = archivo2.read().decode("utf-8")
+        
+        # Se comparan las líneas de los archivos uno a uno
         for linea1, linea2 in zip(archivo1, archivo2):
+            # Si se encuentra una diferencia entre las líneas, se imprime el resultado y se sale del bucle
             if linea1 != linea2:
                 print(diff, "nok")
                 break
         else:
+            # Si no se encontraron diferencias entre las líneas de los archivos, se imprime el resultado
             print(diff, "ok")
