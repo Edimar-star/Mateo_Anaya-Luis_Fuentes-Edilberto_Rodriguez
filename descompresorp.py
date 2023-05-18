f8d5637bdd98a3fa17a46c75750cb3586c692e4f
Edimar-star <edimarod@outlook.es>
2023-05-17 21:36:37 -0500

    all is ok

--- descompresorp.py
+++ descompresorp.py
@@ -1,20 +1,15 @@
-from trie import insert_prefixes, remove_prefixes
 from huffman import HuffmanNode
 from mpi4py import MPI
 import numpy as np
 import time
 import sys
 
-# Función para decodificar el texto utilizando el árbol de Huffman
 def decode_text(encoded_text, root):
     decoded_text = ''
     node = root
 
     for bit in encoded_text:
-        if bit == '0':
-            node = node.left
-        else:
-            node = node.right
+        node = node.left if bit == '0' else node.right
 
         if node.char is not None:
             decoded_text += node.char
@@ -22,43 +17,49 @@ def decode_text(encoded_text, root):
 
     return decoded_text
 
+def find_error(encoded_text, root):
+    value = ''
+    node = root
+
+    for bit in encoded_text:
+        node = node.left if bit == '0' else node.right
+
+        value += bit
+        if node.char is not None:
+            value = ''
+            node = root
+
+    return value
+
 def huffman_decompress(encoded_array, root):
     decoded_text = decode_text(encoded_array, root)
     return decoded_text
 
-def build_codeword_table(root):
-    codes = {}
-    stack = [(root, "")]
-    while stack:
-        node, code = stack.pop()
-        if node.char is not None:
-            codes[node.char] = code
-        else:
-            stack.append((node.left, code + "0"))
-            stack.append((node.right, code + "1"))
-
-    return codes
-
-def eliminar_repetidos(cadena, trieRoot):
+def divide_encode_string(cadena, root):
     if len(cadena) <= 500:
-        return remove_prefixes(cadena, trieRoot)
+        return find_error(cadena, root)
 
     mitad = len(cadena) // 2
-    result1 = eliminar_repetidos(cadena[:mitad], trieRoot)
-    result2 = eliminar_repetidos(result1 + cadena[mitad:], trieRoot)
+    result1 = divide_encode_string(cadena[:mitad], root)
+    result2 = divide_encode_string(result1 + cadena[mitad:], root)
 
     return result2
 
-def sendPackage(end_index, start_index, length_packages, residue, compressed, root, packages_sended, dest, trieRoot):
-    end_index = start_index + length_packages + residue
-    temp_compressed = compressed[start_index:end_index]
-    residue = len(eliminar_repetidos(temp_compressed, trieRoot))
-    end_index = end_index - residue
-    comm.send((root, temp_compressed[:end_index], packages_sended), dest=dest)
-    start_index = end_index
-    packages_sended += 1
+def divide_string(string, n):
+    length = len(string)
+    part_size = length // n
+    remainder = length % n
 
-    return end_index, start_index, residue, packages_sended
+    parts = []
+    start = 0
+    for i in range(n):
+        part_length = part_size + (i < remainder)
+        end = start + part_length
+        part = string[start:end]
+        parts.append(part)
+        start = end
+
+    return parts
 
 if __name__ == "__main__":
     comm = MPI.COMM_WORLD
@@ -67,50 +68,39 @@ if __name__ == "__main__":
 
     if rank == 0:
         start_time = time.time()
-        # Load Huffman encoding and compressed text from file
         compressed_filename = sys.argv[1]
         with open(compressed_filename, 'rb') as f:
             compressed = bin(int.from_bytes(np.load(f, allow_pickle=True), byteorder='big'))[2:]
             root = HuffmanNode.from_array(np.load(f, allow_pickle=True))
-            file_format = np.load(f, allow_pickle=True).tobytes().decode()
-            ENCODING = np.load(f, allow_pickle=True).tobytes().decode()
+            file_format, ENCODING = np.load(f, allow_pickle=True).tobytes().decode(), np.load(f, allow_pickle=True).tobytes().decode()
 
-        # Creo el diccionario
-        codeword_dict = build_codeword_table(root)
-        values = sorted(codeword_dict.values(), key=len)
-        trieRoot = insert_prefixes(values)
-
-        # Inicializo variables primordiales
+        root = comm.bcast(root, root=0)
         tam_compressed = len(compressed)
-        length_packages = 100000
+        length_packages = 10000
         total = tam_compressed // length_packages
         residue = tam_compressed % length_packages
-        start_index = 0
-        end_index = 0
+        packages = divide_string(compressed, total)
 
-        # Envio de paquetes iniciales
-        packages_sended = 0
-        for i in range(1, size):
-            values_rcv = sendPackage(end_index, start_index, length_packages, residue, compressed, root, packages_sended, i, trieRoot)
-            end_index, start_index, residue, packages_sended = values_rcv
+        cont = 0
+        stack = [i for i in range(1, size)]
+        while len(packages) > 0:
+            index = stack.pop(0)
+            value = divide_encode_string(packages[0], root)
+            packages[0] = packages[0][:len(packages[0]) - len(value)]
+            if len(packages) > 1:
+                packages[1] = value + packages[1]
+            comm.send((packages.pop(0), cont), dest=index)
+            stack.append(index)
+            cont += 1
 
-        # Recibo los resultados de todos los hijos
-        decompressed = [0] * total
-        tasks_done = 0
-        while tasks_done < packages_sended:
-            result, indice, dest = comm.recv(source=MPI.ANY_SOURCE)
-            decompressed[indice] = result
-            tasks_done += 1
-
-            if start_index < tam_compressed:
-                values_rcv = sendPackage(end_index, start_index, length_packages, residue, compressed, root, packages_sended, dest, trieRoot)
-                end_index, start_index, residue, packages_sended = values_rcv
-        
-        # Aviso a todos los hijos que ya no voy a enviar mas paquetes
+        decompressed = []
         for i in range(1, size):
             comm.send(None, dest=i)
+            decompressed += comm.recv(source=MPI.ANY_SOURCE)
 
-        # Decompress the text
+        decompressed = sorted(decompressed, key=lambda x: x[1])
+        decompressed = np.array(decompressed)[:, 0]
+
         decompressed_filename = f"descomprimidop-elmejorprofesor.{file_format}"
         with open(decompressed_filename, 'wb') as f:
             decoded_text = ''.join(decompressed)
@@ -121,13 +111,16 @@ if __name__ == "__main__":
         print(f"{total_time:.2f}")
 
     else:
-        while True: # Mientras haya paquetes por recibir
+        root = comm.bcast(None, root=0)
+        values = []
+        while True:
             data = comm.recv(source=0)
 
-            # Si no hay paquetes por recibir
             if data is None:
                 break
 
-            root, encoded_array, indice = data
+            encoded_array, indice = data
             decoded_text = huffman_decompress(encoded_array, root)
-            comm.send((decoded_text, indice, rank), dest=0)
+            values.append((decoded_text, indice))
+
+        comm.send(values, dest=0)
